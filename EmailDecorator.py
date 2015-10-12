eefeeddc0a0309d16090bf46f9b644f0d2f14c20
Craig <tingeycraig@gmail.com>
2015-10-11 21:48:47 -0600

    new things

--- EmailDecorator.py
+++ EmailDecorator.py
@@ -1,3 +1,5 @@
+import smtplib
+from email.mime.text import MIMEText
 
 class EmailDecorator(object):
 
@@ -5,3 +7,15 @@ class EmailDecorator(object):
         self.decorated = obs
         self.email = email
         self.decorated = {'email':email,'observer':obs}
+
+    # This function is not completely working.
+    def send_emails(self):
+        fp = open(textfile, 'rb')
+        msg = MIMEText(fp.read())
+        fp.close()
+        msg['Subject'] = 'Email from Bike Race'
+        msg['From'] = 'agro@gmail.com'
+        msg['To'] = self.email
+        s = smtplib.SMTP('localhost')
+        s.sendmail('agro@gmail.com', [self.email], msg.as_string())
+        s.quit()
